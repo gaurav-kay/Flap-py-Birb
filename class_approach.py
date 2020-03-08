e8cb5b81472472202e06ba6b1ee9a8e3f86e2aed
Gaurav Kashyap <gauravandkashyap@gmail.com>
2020-03-08 21:18:49 +0530

    init pipes

--- class_approach.py
+++ class_approach.py
@@ -8,6 +8,7 @@ UPDATE_DELAY = 100
 PIPE_GAP = PLAYER_RADIUS * 2.5
 PIPE_WIDTH = 40
 INTER_PIPE_DISTANCE = 50
+PIPE_SPEED = 3
 
 
 class Birb:
@@ -46,29 +47,50 @@ class Pipe:
     pipes = []
 
     def __init__(self, top_left_x, top_left_y):
-        self.top_left_x = self.top_right_x = top_left_x
-        self.top_left_y = top_left_y
-        self.top_right_y = top_left_y + PIPE_WIDTH
-        self.bottom_left_x = self.bottom_right_x = self.top_right_x + PIPE_GAP
-        self.bottom_left_y = top_left_y + PIPE_GAP
-        self.bottom_right_y = self.bottom_left_y + PIPE_GAP + PIPE_WIDTH
+        self.top_left_x = top_left_x
+        self.top_left_y = self.top_right_y = top_left_y
+        self.top_right_x = top_left_x + PIPE_WIDTH
+        self.bottom_left_x = top_left_x
+        self.bottom_left_y = self.bottom_right_y = top_left_y + INTER_PIPE_DISTANCE
+        self.bottom_right_x = self.top_right_x
+        # self.top_right_y = top_left_y + PIPE_WIDTH
+        # self.bottom_left_x = self.bottom_right_x = self.top_right_x + PIPE_GAP
+        # self.bottom_left_y = top_left_y + PIPE_GAP
+        # self.bottom_right_y = self.bottom_left_y + PIPE_GAP + PIPE_WIDTH
         self.off_screen = False
 
     def update(self, win):
-        pass
+        self.top_left_x -= PIPE_SPEED
+        self.top_right_x -= PIPE_SPEED
+        self.bottom_left_x -= PIPE_SPEED
+        self.bottom_right_x -= PIPE_SPEED
+
+        self.draw(win)
+        # if self.top_right_y <= 0:
+        #     print("deleted")
+        #     self.off_screen = True
+        #     Pipe.pipes.remove(self)
+        #     del self
 
     def draw(self, win):
-        pass
+        # pygame.draw.rect(win, (0, 255, 0), (self.top_right_x, self.top_right_y, 0, self.top_left_y))
+        # print(0, self.top_left_y, self.top_right_x, self.top_right_y)
+        # pygame.draw.rect(win, (0, 255, 0), (WIN_HEIGHT, self.bottom_right_y, self.bottom_left_x, self.bottom_left_y))
+        # print(self.bottom_left_x, self.bottom_left_y, WIN_HEIGHT, self.bottom_right_y)
+        pygame.draw.rect(win, (0, 255, 0), (self.top_left_x, 0, PIPE_WIDTH, self.top_right_y))
+        print(0, self.top_left_y, self.top_right_x, self.top_right_y)
+        pygame.draw.rect(win, (0, 255, 0), (self.bottom_left_x, self.bottom_left_y, PIPE_WIDTH, WIN_HEIGHT - self.bottom_right_y))
+        print(self.bottom_left_x, self.bottom_left_y, WIN_HEIGHT, self.bottom_right_y)
 
     @staticmethod
-    def add():
+    def add_pipe():
         pass
 
     @staticmethod
     def init_pipes():
         top_value = randint(20, WIN_HEIGHT - 20 - PIPE_GAP)
 
-        Pipe.pipes.append(Pipe(top_value, WIN_WIDTH))
+        Pipe.pipes.append(Pipe(WIN_WIDTH, top_value))
 
         # Pipe.pipes.append(Pipe(
         #     (top_value, top_value + PIPE_WIDTH),
@@ -79,8 +101,10 @@ class Pipe:
             top_value = randint(20, WIN_HEIGHT - 20 - PIPE_GAP)
 
             Pipe.pipes.append(Pipe(
-                top_value,
-                Pipe.pipes[-1].top_left_y + PIPE_WIDTH + INTER_PIPE_DISTANCE
+                # top_value,
+                # Pipe.pipes[-1].top_left_y + PIPE_WIDTH + INTER_PIPE_DISTANCE
+                Pipe.pipes[-1].top_right_x + PIPE_WIDTH + INTER_PIPE_DISTANCE,
+                top_value
             ))
 
 
@@ -92,7 +116,10 @@ pygame.display.set_caption("Flap-py birb")
 birb1 = Birb()
 birb2 = Birb()
 
+Pipe.init_pipes()
+
 while True:  # until game window is open. sort of like a game window driver
+    win.fill((0, 0, 0))
     pygame.time.delay(UPDATE_DELAY)
 
     for event in pygame.event.get():
@@ -106,15 +133,17 @@ while True:  # until game window is open. sort of like a game window driver
             if event.key == pygame.K_DOWN:
                 birb2.jump()
 
-    Pipe.init_pipes()
-
     if len(Pipe.pipes) < 5:
-        Pipe.add()
+        Pipe.add_pipe()
+
+    print(len(Pipe.pipes))
 
     for pipe in Pipe.pipes:
         pipe.update(win)
 
-    win.fill((0, 0, 0))
+    # delete pipes off screen
+    # Pipe.delete_pipes()
+
     birb1.update(win)
     birb2.update(win)
 
